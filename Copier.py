71aa62ea1f9a86e0ea96fcc5656fd25dfbbf22f3
Aquarious <aquarious0696@gmail.com>
2019-10-16 10:56:44 +0300

    Update Copier.py

--- Copier.py
+++ Copier.py
@@ -1,6 +1,9 @@
 from shutil import copy
 from time import sleep
+from datetime import datetime
+import pickle
 import os
+
 
 class File:
     def __init__(self, name=None, path_to_copy=None, path_copy_from=None, modification_time=0):
@@ -11,17 +14,17 @@ class File:
         self.full_name = '{}/{}'.format(self.path_copy_from, self.name)
 
     @staticmethod
-    def copy_file(file_name, path_copy_from, path_copy_to):
+    def copy_file(file_name, copy_from, copy_to):
         """
         Copy file FROM one path TO another
         :param file_name: file name (not relative)
-        :param path_copy_from:
-        :param path_copy_to:
+        :param copy_from:
+        :param copy_to:
         :return:
         """
-        if None not in [file_name, path_copy_from, path_copy_to]:
-            full_name = '{}/{}'.format(path_copy_from, file_name)
-            copy(full_name, path_copy_to)
+        if None not in [file_name, copy_from, copy_to]:
+            full_name = '{}/{}'.format(copy_from, file_name)
+            copy(full_name, copy_to)
         else:
             raise NameError
 
@@ -57,15 +60,27 @@ class Copier:
         self.files_list = to_list(files)
 
     def create_back_up(self):
-
+        for file in self.files_list:
+            back_up_time = str(datetime.today())[0:19]  # Without ms
+            back_up_time = back_up_time.replace(':', '.')  # ":" are not allowed in file_name
+            file.path_copy_to += '/back_up/{}'.format(back_up_time)
+            file.copy_to()
+            file.path_copy_to = file.path_copy_to.replace('/back_up/{}'.format(back_up_time), '')
 
     def watch(self, period=60):
+        with open('files_list', 'rb') as f:
+            self.files_list = pickle.load(f)
+
         while True:
             for file in self.files_list:
                 if file.modified():
                     file.copy_to()
                     print('file {} was duplicated in {}'.format(file.name, file.modification_time))
             sleep(period)
+
+    def save_list(self):
+        with open('files_list', 'wb') as f:
+            pickle.dump(self.files_list, f)
 
 
 def to_list(data):
