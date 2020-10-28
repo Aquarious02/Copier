0192c68a5270eb7277d41a7dc3431f01fbd1a3c6
Aquarious <aquarious0696@gmail.com>
2020-10-28 15:41:42 +0300

    Path.json creating

--- Copier.py
+++ Copier.py
@@ -1,10 +1,14 @@
+import json
+
 from Copier_Lib import *
 
-with open('dir_config.txt', 'r') as f:
-    dirs = []
-    for line in f:
-        dirs.append(line.replace('\n', ''))
-    copy_from, copy_to = dirs
 
-my_copier = DirCopier(copy_from, copy_to)
-my_copier.watch(period=60)
+with open('Paths.json', 'r') as f:
+    dirs = json.load(f)['mods']
+
+dir_copier = DirCopier(dirs['copy files from'], dirs['copy files to'])
+file_copier = FileCopier(dirs['absolute file path'], dirs['copy file to'])
+
+# Uncomment dir or file copier to backup dir with files or one file only
+# dir_copier.watch(period=60)
+# file_copier.watch(period=60)
