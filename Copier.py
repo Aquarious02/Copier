8e377d49b5ccd60eaa2d764ae83add3ab13b5798
Aquarious <aquarious0696@gmail.com>
2020-10-29 15:47:25 +0300

    Auto detect directories

--- Copier.py
+++ Copier.py
@@ -2,13 +2,19 @@ import json
 
 from Copier_Lib import *
 
+try:
+    with open('Paths.json', 'r') as f:
+        dirs = json.load(f)
+except FileNotFoundError:
+    with open('Paths.json', 'w') as f:
+        json.dump({"copy files from": "", "copy files to":  "",
+                   "absolute file path":  "", "copy file to":  ""}, f)
 
-with open('Paths.json', 'r') as f:
-    dirs = json.load(f)['mods']
-
-dir_copier = DirCopier(dirs['copy files from'], dirs['copy files to'])
-file_copier = FileCopier(dirs['absolute file path'], dirs['copy file to'])
-
-# Uncomment dir or file copier to backup dir with files or one file only
-# dir_copier.watch(period=60)
-# file_copier.watch(period=60)
+if dirs['copy files from'] != '' or dirs['copy files to'] != '':
+    dir_copier = DirCopier(dirs['copy files from'], dirs['copy files to'])
+    dir_copier.watch(period=60)
+elif dirs['absolute file path'] != '' or dirs['copy file to'] != '':
+    file_copier = FileCopier(dirs['absolute file path'], dirs['copy file to'])
+    file_copier.watch(period=60)
+else:
+    print('There is no dirs to copy')
